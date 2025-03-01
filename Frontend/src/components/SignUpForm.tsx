d23745910831706a4fcb1bbbe35781eb34e41d9a
Kartik Gothwal <kartikgothwal28@gmail.com>
2025-03-02 01:13:43 +0530

    remove the unused

--- Frontend/src/components/SignUpForm.tsx
+++ Frontend/src/components/SignUpForm.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
 import { z } from "zod";
